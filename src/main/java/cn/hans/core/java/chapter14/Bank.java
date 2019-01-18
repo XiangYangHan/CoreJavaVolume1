3f706a3e0451965b8c1aad44f52d602df0c81818
XiangYangHan <hans.2015soar@gmail.com>
2019-01-18 23:18:10 +0800

    添加Lock后，安全的操作共享变量

--- src/main/java/cn/hans/core/java/chapter14/Bank.java
+++ src/main/java/cn/hans/core/java/chapter14/Bank.java
@@ -1,10 +1,14 @@
 package cn.hans.core.java.chapter14;
 
 import java.util.Arrays;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Bank {
 
     private final double[] accounts;
+
+    private Lock bankLock = new ReentrantLock();
 
     public Bank(int n, double initialBalance) {
         accounts = new double[n];
@@ -12,16 +16,20 @@ public class Bank {
     }
 
     public void transfer(int form, int to, double amount) throws InterruptedException {
-        if (accounts[form] < amount) {
-            return;
+        bankLock.lock();
+        try {
+            if (accounts[form] < amount) {
+                return;
+            }
+            System.out.print(Thread.currentThread());
+            accounts[form] -= amount;
+            System.out.printf(" %10.2f from %d to %d", amount, form, to);
+//            Thread.sleep(5);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance: %10.2f\n", getTotalBalance());
+        } finally {
+            bankLock.unlock();
         }
-        System.out.print(Thread.currentThread());
-        accounts[form] -= amount;
-        System.out.printf(" %10.2f from %d to %d", amount, form, to);
-        Thread.sleep(5);
-        accounts[to] += amount;
-        System.out.printf(" Total Balance: %10.2f\n", getTotalBalance());
-
     }
 
     public double getTotalBalance() {
